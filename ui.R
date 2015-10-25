ea8ec4dd181e1749a8fc2bd07340158d12b04edb
ksreems <kausar.shaik@hotmail.com>
2015-10-26 00:47:50 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,21 +1,28 @@
-shinyUI(
-  pageWithSidebar(
-    # Application title
-    headerPanel("BMI Calculation"),
-    # Key in your height (in metres) and weight (in kg) to calculate your BMI
-    sidebarPanel(
-      numericInput('id1', 'Height m', 0, min = 0, max = 2, step = 0.1),
-      numericInput('id2', 'Weight kg', 0, min = 0.0, max = 200, step = 0.1),
-      submitButton('Submit')
+library(shiny)
+library(ggplot2)
+
+mydata <- iris
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Plot of Species of Iris Flowers"),
+  
+  sidebarPanel(
+    
+    selectInput('x', 'Select the data for the x-axis', names(mydata)),
+    selectInput('y', 'Select the data for the y-axis', names(mydata), 
+                names(mydata)[[2]]),
+    selectInput('color', 'To color data by Species or Not', 
+                c('None', names(mydata)[[5]]))    
     ),
-    mainPanel(
-      h3('BMI Result'),
-      h4('You entered'),
-      verbatimTextOutput("oid1"),
-      h4('You entered'),
-      verbatimTextOutput("oid2"),
-      h4('Which resulted in a BMI of '),
-      verbatimTextOutput("calculation")
-    )
+  
+  mainPanel(
+    p("The iris data set gives the measurements in centimeters of the variables 
+      sepal length and width and petal length and width, respectively, for 50 flowers 
+      from each of 3 species of iris. The species are Iris setosa, versicolor, and 
+      virginica."),
+    p("The plot below shows a scatterplot of the measurements of the flowers, which
+      can be color-coded by species."),
+    plotOutput('plot')
   )
-)
+))
